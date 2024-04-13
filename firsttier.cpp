3ba22d8818cb24522915d48a62dfb7c58e0443a9
sujira7236212 <sujira7236212@gmail.com>
2024-04-13 20:28:32 +0700

    menu

--- firsttier.cpp
+++ firsttier.cpp
@@ -5,19 +5,110 @@ using namespace std;
 class first :public BST{
     private:
     public:
-        first();
-        void build();
+        //first();
+        void build(int,int);
+        void search(TreeNodePtr,int,int);
+        void addmenu(string);
+        void menu1();
+        void menu2();
+        void loopmenu();
 };
+// first::first(){
 
-first::first(){
-    //build();
+// }
+
+void first::build(int i,int j){
+    TreeNodePtr t;
+    t = create(6,"Wood", create(1,"Metal",nullptr, nullptr),create(5, "Plant",create(2,"Water",nullptr, nullptr),create(3,"Seeds",nullptr, nullptr)));
+    if(i==2&&j==3 || i==3&&j==2){
+        if((findparent(t,i,j)->data)==5){
+            cout<<"You got "<<findparent(t,2,3)->name<<endl; //Plant
+            
+        }
+        //addmenu("Plant");
+    }
+    else if(i==1&&j==5 || i==5&&j==1){
+        if((findparent(t,i,j)->data)==6){
+            cout<<"You got "<<findparent(t,1,5)->name<<endl; //Wood
+    }
+    }
+    else cout<<"Nothing happen"<<endl;
+    
+
+    //std::cout<<findparent(t,3,5)->data<<" 1"<<endl;
 }
 
-void first::build(){
-    TreeNodePtr t;
+void first::menu1(){
+	int select1;
+	cout<<"Please choose first element"<<endl;
+	int k;
+	string array[20];
+	array[1]="Metal";
+	array[2]="Water";
+	array[3]="Seeds";
+	array[4]="Glass";
+	for(k=1;k<20;k++){
+		if(!array[k].empty()){
+			cout<<k<<".";
+			cout<<array[k]<<endl;
+		}
+		
+	}
+	// cout<<"You choose: "<<endl;
+	// cin>>select1;
+	// return select1;
+	
+}
+
+void first::menu2(){
+	int select2;
+	cout<<"Please choose second element"<<endl;
+	int k;
+	string array[20];
+	array[1]="Metal";
+	array[2]="Water";
+	array[3]="Seeds";
+	array[4]="Glass";
+	for(k=1;k<20;k++){
+		if(!array[k].empty()){
+			cout<<k<<".";
+			cout<<array[k]<<endl;
+		}
+	}
+	// cout<<"You choose: "<<endl;
+	// cin>>select2;
+	// return select2;
+}
+
+void first::addmenu(string add){
+    string array[20];
+    int k = 4;
+    int r;
+    if(k<20){
+        r = ++k;
+        array[r] = add; 
+        cout<<r<<".";
+		cout<<array[r]<<endl;
+    }
+
+}
+
+void first::loopmenu(){
+    int select1;
+    int select2;
+    while(1){
+        menu1();
+        cout<<"You choose: "<<endl;
+	    cin>>select1;
+        menu2(); 
+        cout<<"You choose: "<<endl;
+	    cin>>select2; 
+        build(select1,select2);
+        if(select1==2 && select2==3){
+            addmenu("Plant");
+        }
+    }
     
-    t = create(4,"4", create(3, "3",create(1,"1",nullptr, nullptr),create(2,"2",nullptr, nullptr)),create(5,"5",nullptr, nullptr));
-    std::cout<<findparent(t,3,5)->data<<" 1"<<endl;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -3,9 +3,54 @@
 #include "firsttier.cpp"
 #include <string>
 using namespace std;
+
+// int menu1(){
+// 	int select1;
+// 	cout<<"Please choose first element"<<endl;
+// 	int k;
+// 	string array[20];
+// 	array[1]="Metal";
+// 	array[2]="Water";
+// 	array[3]="Seeds";
+// 	array[4]="Glass";
+// 	for(k=1;k<20;k++){
+// 		if(!array[k].empty()){
+// 			cout<<k<<".";
+// 			cout<<array[k]<<endl;
+// 		}
+		
+// 	}
+// 	cout<<"You choose: "<<endl;
+// 	cin>>select1;
+// 	return select1;
+	
+// }
+// int menu2(){
+// 	int select2;
+// 	cout<<"Please choose second element"<<endl;
+// 	int k;
+// 	string array[20];
+// 	array[1]="Metal";
+// 	array[2]="Water";
+// 	array[3]="Seeds";
+// 	array[4]="Glass";
+// 	for(k=1;k<20;k++){
+// 		if(!array[k].empty()){
+// 			cout<<k<<".";
+// 			cout<<array[k]<<endl;
+// 		}
+		
+// 	}
+// 	cout<<"You choose: "<<endl;
+// 	cin>>select2;
+// 	return select2;
+// }
 int main(){
+	TreeNodePtr t;
 	first a;
-	a.build();
-	//p = tree.createNode(4,"4", tree.createNode(3, tree.createNode(1,nullptr, nullptr), tree.createNode(2,nullptr, nullptr)), tree.createNode(5, nullptr, nullptr));
+	
+	// int i = menu1();
+	// int j = menu2();
+	a.loopmenu();
 	return 0;
 }
