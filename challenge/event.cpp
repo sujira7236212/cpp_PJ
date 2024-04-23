eb73476bd806bad869c90b2bb80a7c4586cab1d4
PS <156405238+PatthinanS@users.noreply.github.com>
2024-04-23 06:48:17 +0000

    event.cpp

--- challenge/event.cpp
+++ challenge/event.cpp
@@ -10,19 +10,19 @@ void event::description() {
 void event::randomEvent(string s) {
 
   if (s == "WOOD") {
-    if (random(5)) {
+    if (random(2)) {
       element = specialElem(1);
     } else {
       blueprint(1);
     }
-  } else if (s == "METAL") {
-    if (random(5)) {
+  } else if (s == "WATER") {
+    if (random(2)) {
       element = specialElem(2);
     } else {
       blueprint(2);
     }
-  } else if (s == "GLASS") {
-    if (random(5)) {
+  } else if (s == "METAL") {
+    if (random(2)) {
       element = specialElem(3);
     } else {
       blueprint(3);
@@ -36,36 +36,60 @@ void event::randomEvent(string s) {
   }
 }
 
+string event::craftable(string s, string spe, string ele){
+  string element1, element2, clean;
+
+  cout<<"You now have the ability to craft a "<<s<<endl
+  <<"Please input a special element and an element: ";<<endl;
+
+  cout<<"1) Special Element: ";
+  cin>>element1;
+  
+  cout<<<<"2) Element: ";
+  cin>>element2;
+
+  //.....
+
+  return clean;
+}
+
 void event::blueprint() {
     int x = rand()%3;
 
     switch (x){
-        case 1: //for waterWheel: wood+wheels
+        case 1: //for waterWheel: wheels+water
             cout<<"Water Wheel: Kill them, get their flesh, make them drive"<<endl;
             break;
-        case 2: //for windMill: metal+rotor
+        case 2: //for windMill: rotor+wood
             cout<<"Wind Mill: ironman spin"<<endl;
             break;
-        case 3: //for solarPanel: silicon+glass
-            cout<<"Solar Panel: Silicon valley sure loves grass."<<endl;
+        case 3: //for solarPanel: silicon+metal
+            cout<<"Solar Panel: Silicon valley sure loves metal."<<endl;
             break;
         default: cout<<"Error";
     }
 
 }
 
-void event::blueprint(int n) {
+void event::blueprint(int n) { //fixed chance of getting blueprint
     int x = n;
 
     switch (x){
-        case 1: //for waterWheel: wood+wheels
-            cout<<"Water Wheel: Kill them, get their flesh, make them drive"<<endl;
+        case 1: //for waterWheel: wheels+water
+          ww = true;
+            cout<<"Water Wheel: Can we drive water?"<<endl;
+            clean_ = craftable("Water Wheel", ,);
+
             break;
-        case 2: //for windMill: metal+rotor
-            cout<<"Wind Mill: ironman spin"<<endl;
+        case 2: //for windMill: rotor+wood
+          wm = true;
+            cout<<"Wind Mill: Tree flesh goes spin."<<endl;
+          
             break;
-        case 3: //for solarPanel: silicon+glass
-            cout<<"Solar Panel: Silicon valley sure loves grass."<<endl;
+        case 3: //for solarPanel: silicon+metal
+          sp = true;
+            cout<<"Solar Panel: Silicon valley sure loves metal."<<endl;
+            
             break;
         default: cout<<"Error";
     }
@@ -74,16 +98,39 @@ void event::blueprint(int n) {
 string event::specialElem(int n){ //return to add element in menu
     switch(n){
         case 1:
-            element = "Wheels";
+          if(blueprintGet(1))
+            element = "x Wheels";
+          else
+            element = "/ Wheels";
             break;
         case 2:
-            element = "Rotor";
+          if(blueprintGet(2))
+            element = "x Rotor"; //solarPanel
+          else
+            element = "/ Rotor";
             break;
         case 3:
-            element = "Silicon";
+        if(blueprintGet(3))
+            element = "x Silicon"; //
+          else
+            element = "/ Silicon";
             break;
         default:
             element = "Nothing Happened.";
     }
     return element;
+}
+
+bool blueprintGet(int n){
+  switch (n){
+        case 1:
+            
+            break;
+        case 2:
+            cout<<"Wind Mill: Tree flesh goes spin."<<endl;
+            break;
+        case 3:
+            cout<<"Solar Panel: Silicon valley sure loves metal."<<endl;
+            break;
+        default: cout<<"Error";
 }
